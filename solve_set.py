2a3f8dac70fa80129a750ad68e43f518cfb9eba0
Nicolas Hahn <nicolas@stonespring.org>
2018-06-28 12:45:41 -0400

    add help msgs to solve_set parser

--- solve_set.py
+++ solve_set.py
@@ -10,22 +10,32 @@ from common import game_img_filename
 
 def get_args():
   parser = argparse.ArgumentParser(description='Solve SET from a game image.')
-  parser.add_argument('filename', metavar='filename', type=str, nargs='?')
-  parser.add_argument('--game', dest='game_num', type=int)
-  parser.add_argument('--write', dest='write', action='store_true')
-  parser.add_argument('--display', dest='display', action='store_true')
+  parser.add_argument('filename', metavar='filename', type=str, nargs='?',
+    help='Game image filename')
+  parser.add_argument('--game', dest='game_num', type=int,
+    help='use a test image from image-data/set-games/setgame<GAME_NUM>.jpg')
+  parser.add_argument('--write', dest='write', action='store_true',
+    help='Write the solved image to solve-out/solved.jpg')
+  parser.add_argument('--display', dest='display', action='store_true',
+    help='Display the solved image with cv2.display()')
   return parser.parse_args()
 
 def main():
   args = get_args()
+
   if args.game_num:
     filename = game_img_filename(args.game_num)
-  else:
+  elif args.filename:
     filename = args.filename
+  else:
+    print 'Error: must pass either a filename or --game GAME_NUM'
+    sys.exit(1)
+
   game = SetGame(filename)
   game.solve()
   game.print_sets()
   game.draw_sets()
+
   if args.write:
     game.write_im('solved.jpg')
   if args.display:
